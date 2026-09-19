e94c0f95c5946ab6d5044b6fcca4cabf344acc10
afghanfollower1 <afghanfollower1@gmail.com>
2026-09-19 09:46:49 +0300

    Prepare profile UI for countries and avatar images

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:country_picker/country_picker.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'core/api_service.dart';
 import 'core/google_auth_service.dart';
