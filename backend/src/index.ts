065d7009718dc42073c1b2973fe0e4104bfbe111
afghanfollower1 <afghanfollower1@gmail.com>
2026-09-21 16:09:25 +0300

    Register Promotions customer API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,6 +25,7 @@ import { registerSocialRoutes } from './socialRoutes.js';
 import { startSocialAutoSync } from './socialSync.js';
 import { registerVirtualNumberRoutes } from './virtualNumberRoutes.js';
 import { registerPremiumRoutes } from './premiumRoutes.js';
+import { registerPromotionRoutes } from './promotionRoutes.js';
 import { normalizeCurrencyCode } from './currency.js';
 import {
   recordReferralRegistration,
@@ -1529,6 +1530,7 @@ registerHesabPayWebhookRoutes(app, prisma, authenticate);
 registerSocialRoutes(app, prisma, authenticate, adminWebUser);
 registerVirtualNumberRoutes(app, prisma, authenticate, adminWebUser);
 registerPremiumRoutes(app, prisma, authenticate);
+registerPromotionRoutes(app, prisma, authenticate);
 registerReferralRoutes(app, prisma, authenticate);
 startSocialAutoSync(prisma, app.log as any);
 startNotificationPushScheduler(prisma, app.log as any);
